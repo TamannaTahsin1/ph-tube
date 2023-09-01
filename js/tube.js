488a47d1effdd17e0c18b8f2793ffe357e645745
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-09-02 04:55:24 +0600

    drawing section added

--- js/tube.js
+++ js/tube.js
@@ -2,7 +2,7 @@ const handleCategory = async () =>{
    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
    const data = await res.json();
    const categories = data.data;
-   console.log(categories);
+   
     // for buttons
    const btnContainer = document.getElementById('btn-container');
    categories.forEach((category) => {
@@ -19,14 +19,24 @@ const handleCategory = async () =>{
         const data = await res.json();
         const trimmedData = data.data;
 
+        // drawing
+        const drawingContainer = document.getElementById('drawing-container');
+
+        if (trimmedData.length === 0) {
+            drawingContainer.classList.remove('hidden');
+        }
+        else {
+            drawingContainer.classList.add('hidden');
+        }
+
         const cardContainer = document.getElementById('card-container')
         cardContainer.innerHTML = "";
         trimmedData.forEach((news) =>{
             const div= document.createElement('div')
             div.innerHTML = 
-            `<div class="card md:w-80 md:h-80 lg:w-96 lg:h-96 bg-base-100 shadow-xl m-5 ">
-            <figure><img src="${news?.thumbnail}" alt="card" class="w-96"/>          
-            <p class="absolute flex justify-center md:top-20 lg:top-44 lg:left-56 bg-gray-400 rounded-xl text-black text-xs w-28 font-semibold">${secondsToHour(news?.others?.posted_date)}</p>           
+            `<div class="card w-80 md:w-72 md:h-80 lg:w-80 lg:h-96 bg-base-100 shadow-xl m-5 ">
+            <figure><img src="${news?.thumbnail}" alt="card" class="w-80 h-48"/>          
+            <p class="absolute flex justify-center md:top-38 lg:top-36 lg:left-40 bg-gray-400 rounded-xl text-black text-xs w-28 font-semibold">${secondsToHour(news?.others?.posted_date)}</p>           
             </figure>
             <div class="card-body">
                 <div class="card-footer flex mt-2 gap-4">
@@ -39,7 +49,7 @@ const handleCategory = async () =>{
                     <p>${news?.authors[0].profile_name}</p>
                     <p>${news.authors[0].verified ? '<img src="./img/var.svg" class="mr-28 ">' : " "}</p>                  
                 </div>
-                <p class="ml-16">${news?.others?.news}</p>
+                <p class="ml-16">${news?.others?.views}</p>
           </div>
     </div>`;
     cardContainer.appendChild(div);
